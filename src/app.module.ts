6fc040b939751e0fbdd764ed741ee50d69294826
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-06-03 15:23:23 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
